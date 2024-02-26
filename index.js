f1d967f45cb46a42bb0ce84cdbf453e3ce2d65eb
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2024-02-26 12:54:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
 
 conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
